59de8309a2c2fb37b184e27f9347e51c1d5bf7de
Niall <NiallHarrington12@gmail.com>
2022-02-16 14:41:25 +0700

    First commit, code complete already. Will do README next.

---
+++ app.js
@@ -0,0 +1,45 @@
+
+const straight = document.querySelector('.straight')
+const ninety = document.querySelector('.ninety')
+const oneEighty = document.querySelector('.oneEighty')
+const twoSeventy = document.querySelector('.twoSeventy')
+
+
+// Add event listener
+straight.addEventListener('click', () => {
+    if(straight.classList.contains('on')) {
+        ninety.classList.add('on');
+        straight.classList.remove('on');
+    }
+})
+ninety.addEventListener('click', () => {
+    if(ninety.classList.contains('on')) {
+        oneEighty.classList.add('on');
+        ninety.classList.remove('on');
+    }
+})
+oneEighty.addEventListener('click', () => {
+    if(oneEighty.classList.contains('on')) {
+        twoSeventy.classList.add('on');
+        oneEighty.classList.remove('on');
+    }
+})
+twoSeventy.addEventListener('click', () => {
+    if(twoSeventy.classList.contains('on')) {
+        straight.classList.add('on');
+        twoSeventy.classList.remove('on');
+    }
+})
+/*closedFace.addEventListener('click', () => {
+    if(clockwise.classList.contains('oneEighty')) {
+        clockwise.classList.add('twoSeventy');
+        clockwise.classList.remove('oneEighty');
+    }
+})
+closedFace.addEventListener('click', () => {
+    if(clockwise.classList.contains('twoSeventy')) {
+        clockwise.classList.add('straight');
+        clockwise.classList.remove('twoSeventy');
+    }
+})
+ */
