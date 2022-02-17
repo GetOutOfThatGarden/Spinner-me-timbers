82e042c2b4a33a1958939d49644067f94da02541
obinasBaba <henokgetachew500@gmail.com>
2022-02-17 14:43:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 
 const straight = document.querySelector('.straight')
-const ninety = document.querySelector('.ninety')
-const oneEighty = document.querySelector('.oneEighty')
-const twoSeventy = document.querySelector('.twoSeventy')
+// const ninety = document.querySelector('.ninety')
+// const oneEighty = document.querySelector('.oneEighty')
+// const twoSeventy = document.querySelector('.twoSeventy')
 
 
 // Add event listener
 straight.addEventListener('click', () => {
-    if(straight.classList.contains('on')) {
-        ninety.classList.add('on');
-        straight.classList.remove('on');
-    }
+//     if(straight.classList.contains('on')) {
+//         ninety.classList.add('on');
+//         straight.classList.remove('on');
+//     }
+    
+     e.currentTarget.classList.toggle('rotate')
+  
 })
-ninety.addEventListener('click', () => {
-    if(ninety.classList.contains('on')) {
-        oneEighty.classList.add('on');
-        ninety.classList.remove('on');
-    }
+
+//remove the 'rotate' class when animation finishes
+straight.addEventListener('animationend', e => {
+        e.currentTarget.classList.remove('rotate')
 })
-oneEighty.addEventListener('click', () => {
-    if(oneEighty.classList.contains('on')) {
-        twoSeventy.classList.add('on');
-        oneEighty.classList.remove('on');
-    }
-})
-twoSeventy.addEventListener('click', () => {
-    if(twoSeventy.classList.contains('on')) {
-        straight.classList.add('on');
-        twoSeventy.classList.remove('on');
-    }
-})
+
+// ninety.addEventListener('click', () => {
+//     if(ninety.classList.contains('on')) {
+//         oneEighty.classList.add('on');
+//         ninety.classList.remove('on');
+//     }
+// })
+// oneEighty.addEventListener('click', () => {
+//     if(oneEighty.classList.contains('on')) {
+//         twoSeventy.classList.add('on');
+//         oneEighty.classList.remove('on');
+//     }
+// })
+// twoSeventy.addEventListener('click', () => {
+//     if(twoSeventy.classList.contains('on')) {
+//         straight.classList.add('on');
+//         twoSeventy.classList.remove('on');
+//     }
+// })
 /*closedFace.addEventListener('click', () => {
     if(clockwise.classList.contains('oneEighty')) {
         clockwise.classList.add('twoSeventy');
